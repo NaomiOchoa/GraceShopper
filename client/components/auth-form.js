b6bfffbd2fd2008161a84d406ff6e8f95cd4d52f
Naomi Ochoa <naomi.m.ochoa@gmail.com>
2020-12-29 21:52:15 -0500

    refactor login and signup styling

--- client/components/auth-form.js
+++ client/components/auth-form.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
 import {auth} from '../store'
+import {Button, Form, Input} from 'semantic-ui-react'
 
 /**
  * COMPONENT
@@ -11,144 +12,81 @@ const AuthForm = props => {
   if (name === 'login') {
     return (
       <div className="main-content-section">
-        <form onSubmit={handleSubmit} name={name}>
-          <div>
-            <label htmlFor="email">
-              <small>Email</small>
-            </label>
-            <input name="email" type="text" />
-          </div>
-          <div>
-            <label htmlFor="password">
-              <small>Password</small>
-            </label>
-            <input name="password" type="password" />
-          </div>
-          <div>
-            <button type="submit">{displayName}</button>
-          </div>
+        <Form onSubmit={handleSubmit} name={name}>
+          <Form.Group>
+            <Form.Field>
+              <label>Email</label>
+              <input
+                placeholder="Email"
+                id="form-input-control-email"
+                name="email"
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>Password</label>
+              <input
+                placeholder="Password"
+                id="form-input-control-password"
+                type="password"
+                name="password"
+              />
+            </Form.Field>
+          </Form.Group>
+          <Form.Field>
+            <Button type="submit">{displayName}</Button>
+          </Form.Field>
           {error && error.response && <div> {error.response.data} </div>}
-        </form>
+        </Form>
         {/* <a href="/auth/google">{displayName} with Google</a> */}
       </div>
     )
   } else if (name === 'signup') {
     return (
       <div className="main-content-section">
-        <form onSubmit={handleSubmit} name={name}>
-          <div>
-            <label htmlFor="firstName">
-              <small>First Name</small>
-            </label>
-            <input name="firstName" type="text" />
-          </div>
-          <div>
-            <label htmlFor="lastName">
-              <small>Last Name</small>
-            </label>
-            <input name="lastName" type="text" />
-          </div>
-          <div>
-            <label htmlFor="email">
-              <small>Email</small>
-            </label>
-            <input name="email" type="text" />
-          </div>
-          <div>
-            <label htmlFor="password">
-              <small>Password</small>
-            </label>
-            <input name="password" type="password" />
-          </div>
-          <div>
-            <label htmlFor="birthday">
-              <small>Birthday</small> <br />
-              <small>month/day/year</small>
-            </label>
-            <input name="birthday" type="text" defaultValue="1/1/1970" />
-          </div>
-          <div>
-            <label htmlFor="address">
-              <small>Address</small>
-            </label>
-            <input name="address" type="text" />
-          </div>
-          <div>
-            <label htmlFor="state">
-              <small>State</small>
-            </label>
-            <select name="state" type="text">
-              <option>AL</option>
-              <option>AK</option>
-              <option>AZ</option>
-              <option>AR</option>
-              <option>CO</option>
-              <option>CA</option>
-              <option>CT</option>
-              <option>DE</option>
-              <option>FL</option>
-              <option>GA</option>
-              <option>HI</option>
-              <option>ID</option>
-              <option>IL</option>
-              <option>IN</option>
-              <option>IA</option>
-              <option>KS</option>
-              <option>KY</option>
-              <option>LA</option>
-              <option>ME</option>
-              <option>MD</option>
-              <option>MA</option>
-              <option>MI</option>
-              <option>MN</option>
-              <option>MS</option>
-              <option>MO</option>
-              <option>MD</option>
-              <option>NE</option>
-              <option>NV</option>
-              <option>NH</option>
-              <option>NJ</option>
-              <option>NM</option>
-              <option>NY</option>
-              <option>NC</option>
-              <option>ND</option>
-              <option>OH</option>
-              <option>OK</option>
-              <option>OR</option>
-              <option>PA</option>
-              <option>RI</option>
-              <option>SC</option>
-              <option>SD</option>
-              <option>TN</option>
-              <option>TX</option>
-              <option>UT</option>
-              <option>VT</option>
-              <option>VA</option>
-              <option>WA</option>
-              <option>WI</option>
-              <option>WV</option>
-              <option>WY</option>
-              <option>DC</option>
-            </select>
-          </div>
-          <div>
-            <label htmlFor="zipCode">
-              <small>Zip Code</small>
-            </label>
-            <input name="zipCode" type="text" />
-          </div>
-          <div>
-            <label htmlFor="country">
-              <small>Country</small>
-            </label>
-            <input name="country" type="text" />
-          </div>
-          <div>
-            <button type="submit">{displayName}</button>
-          </div>
+        <Form onSubmit={handleSubmit} name={name}>
+          <Form.Group>
+            <Form.Field>
+              <label>First Name</label>
+              <input
+                name="firstName"
+                placeholder="First Name"
+                id="form-input-control-first-name"
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>Last Name</label>
+              <input
+                name="lastName"
+                placeholder="Last Name"
+                id="form-input-control-last-name"
+              />
+            </Form.Field>
+          </Form.Group>
+          <Form.Group>
+            <Form.Field>
+              <label>Email</label>
+              <input
+                placeholder="Email"
+                id="form-input-control-email"
+                name="email"
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>Password</label>
+              <input
+                placeholder="Password"
+                id="form-input-control-password"
+                type="password"
+                name="password"
+              />
+            </Form.Field>
+          </Form.Group>
+          <Form.Field>
+            <Button type="submit">{displayName}</Button>
+          </Form.Field>
           {error && error.response && <div> {error.response.data} </div>}
-        </form>
-        <a href="/auth/google">{displayName} with Google</a>
+        </Form>
+        {/* <a href="/auth/google">{displayName} with Google</a> */}
       </div>
     )
   }
@@ -193,12 +131,7 @@ const mapDispatch = dispatch => {
       email: evt.target.email.value,
       password: evt.target.password.value,
       firstName: evt.target.firstName.value,
-      lastName: evt.target.lastName.value,
-      birthday: Date.parse(`${evt.target.birthday.value} 00:00:00 GMT`),
-      address: evt.target.address.value,
-      country: evt.target.country.value,
-      state: evt.target.state.value,
-      zipCode: evt.target.zipCode.value
+      lastName: evt.target.lastName.value
     }
     const formName = evt.target.name
     dispatch(auth(formName, userInfo))
